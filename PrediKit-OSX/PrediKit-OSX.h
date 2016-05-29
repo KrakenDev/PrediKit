540163e9e88eaf2ff3f02eb303e5bc08ed7abf66
Hector Matos <hectormatos2011@gmail.com>
2016-05-28 20:41:10 -0700

    Aco Taco. I touted OSX support but didn't actua;;y do it. Whoopsies...that should be remedied now

---
+++ PrediKit-OSX/PrediKit-OSX.h
@@ -0,0 +1,19 @@
+//
+//  PrediKit-OSX.h
+//  PrediKit-OSX
+//
+//  Created by Hector Matos on 5/28/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PrediKit-OSX.
+FOUNDATION_EXPORT double PrediKit_OSXVersionNumber;
+
+//! Project version string for PrediKit-OSX.
+FOUNDATION_EXPORT const unsigned char PrediKit_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrediKit_OSX/PublicHeader.h>
+
+
