0260fed226ca4c889f360bbe5f5f4790f5088649
Hector Matos <hectormatos2011@gmail.com>
2016-05-30 08:55:28 -0700

    adds support for watchOS and tvOS

---
+++ Supporting Files/tvOS/PrediKit.h
@@ -0,0 +1,19 @@
+//
+//  PrediKit-tvOS.h
+//  PrediKit-tvOS
+//
+//  Created by Hector Matos on 5/30/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrediKit-tvOS.
+FOUNDATION_EXPORT double PrediKit_tvOSVersionNumber;
+
+//! Project version string for PrediKit-tvOS.
+FOUNDATION_EXPORT const unsigned char PrediKit_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrediKit_tvOS/PublicHeader.h>
+
+
---
+++ Supporting Files/watchOS/PrediKit.h
@@ -0,0 +1,19 @@
+//
+//  PrediKit-watchOS.h
+//  PrediKit-watchOS
+//
+//  Created by Hector Matos on 5/30/16.
+//
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for PrediKit-watchOS.
+FOUNDATION_EXPORT double PrediKit_watchOSVersionNumber;
+
+//! Project version string for PrediKit-watchOS.
+FOUNDATION_EXPORT const unsigned char PrediKit_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrediKit_watchOS/PublicHeader.h>
+
+
